8386041a2534616d6036cd79ee1124ad0c12c625
Ben Nadel <ben@bennadel.com>
2014-05-29 08:23:38 -0400

    Make tweet a bit more fun.

--- js/controllers/app-controller.js
+++ js/controllers/app-controller.js
@@ -92,7 +92,7 @@ app.controller(
 		function getTweetText() {
 
 			// TODO: Fix this link once blog post is written.
-			var text = ( "Woot! I just scored " + $scope.score + " points on #RegExDay 2014! Play for a chance to win! http://bjam.in/9999" );
+			var text = ( "Rock on with my bad self! I just scored " + $scope.score + " points on #RegExDay 2014! Play for a chance to win! http://bjam.in/9999" );
 
 			return( encodeURIComponent( text ) );
 
