5a12e319c9da22661757a9c9a0cb57420ca3aedb
Ben Nadel <ben@bennadel.com>
2014-05-30 10:38:55 -0400

    Add alert for unknown words.

--- js/controllers/app-controller.js
+++ js/controllers/app-controller.js
@@ -47,7 +47,7 @@ app.controller(
 			// Ignore any partial submits (based on the list of English words we have).
 			if ( ! dictionaryService.isWord( word ) ) {
 
-				return;
+				return( alert( "Sorry, I don't understand that world." ) );
 
 			}
 
